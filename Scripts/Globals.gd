677dfb0e6bc6db987b0106c775d50152c6a32da6
Jayden Kelly <jaykelly924@gmail.com>
2024-10-20 02:17:10 -0400

    added tower info array

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -8,12 +8,53 @@ signal goldChanged(newGold)
 signal waveStarted(wave_count, enemy_count)
 @warning_ignore("unused_signal")
 signal enemyDestroyed(remain)
+	
+var tower_information = {
+	1: {
+		name = "Sprinkler",
+		id = "sprinkler",
+		price = 80.0,
+		texture = load("res://Textures/Tower/sprinkler/0.png"),
+		round_unlocked = 0
+	},
+	2: {
+		name = "Bench",
+		id = "bench",
+		price = 100.0,
+		texture = load("res://Textures/Tower/bench/top.png"),
+		round_unlocked = 2
+	},
+	3: {
+		name = "Spikes",
+		id = "spike",
+		price = 100.0,
+		texture = load("res://Textures/Tower/spike/3.png"),
+		round_unlocked = 2
+	},
+	4: {
+		name = "Speaker",
+		id = "speaker",
+		price = 200.0,
+		texture = load("res://Textures/Tower/speaker/0.png"),
+		round_unlocked = 3
+	},
+	5: {
+		name = "Police (Bullets)",
+		id = "bullet_cop",
+		price = 200.0,
+		texture = load("res://Textures/Tower/police/bullet_tower.png"),
+		round_unlocked = 5
+	},
+	6: {
+		name = 
+	}
+}
 
 var tower_radius_stats = {
 	"sprinkler": {
 		damage = 1,
-		range = 20,
-		cooldown = 0.1,
+		range = 2,
+		cooldown = 3,
 	},
 	"bullets": {
 		damage = 1,
