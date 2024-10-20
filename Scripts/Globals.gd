a7aeade3187275e70fa77089c76b961a2b58a3fb
Jack8701 <jcarluccio2006@gmail.com>
2024-10-20 05:08:34 -0400

    tower selection

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -3,59 +3,91 @@ extends Node
 @warning_ignore("unused_signal")
 signal hpChanged(newHp, maxHp)
 @warning_ignore("unused_signal")
-signal goldChanged(newGold)
+signal budget_changed(newGold)
 @warning_ignore("unused_signal")
 signal waveStarted(wave_count, enemy_count)
 @warning_ignore("unused_signal")
 signal enemyDestroyed(remain)
 
-var tower_information = {
-	1: {
+var tower_information = [
+	{
 		name = "Sprinkler",
 		id = "sprinkler",
 		price = 80.0,
 		texture = load("res://Textures/Tower/sprinkler/0.png"),
 		round_unlocked = 0
 	},
-	2: {
+	{
 		name = "Bench",
 		id = "bench",
 		price = 100.0,
 		texture = load("res://Textures/Tower/bench/top.png"),
 		round_unlocked = 2
 	},
-	3: {
+	{
 		name = "Spikes",
 		id = "spike",
 		price = 100.0,
 		texture = load("res://Textures/Tower/spike/3.png"),
 		round_unlocked = 2
 	},
-	4: {
+	{
 		name = "Speaker",
 		id = "speaker",
 		price = 200.0,
 		texture = load("res://Textures/Tower/speaker/0.png"),
 		round_unlocked = 3
 	},
-	5: {
+	{
 		name = "Police (Bullets)",
 		id = "bullet_cop",
 		price = 200.0,
 		texture = load("res://Textures/Tower/police/bullet_tower.png"),
 		round_unlocked = 5
 	},
-	6: {
-		
+	{
+		name = "Tear Gas",
+		id = "gas_cop",
+		price = 200.0,
+		texture = load("res://Textures/Tower/police/gas_tower.png"),
+		round_unlocked = 6,
+	},
+	{
+		name = "Tent",
+		id = "tent",
+		price = 100.0,
+		texture = load("res://Textures/Tower/tent.png"),
+		round_unlocked = 8,
+	},
+	{
+		name = "Shelter",
+		id = "shelter",
+		price = 100.0,
+		texture = load("res://Textures/Tower/shelter.png"),
+		round_unlocked = 1,
+	},
+	{
+		name = "Food Bank",
+		id = "food_bank",
+		price = 100.0,
+		texture = load("res://Textures/Tower/food_bank.png"),
+		round_unlocked = 1,
+	},
+	{
+		name = "Houses",
+		id = "houses",
+		price = 100.0,
+		texture = load("res://Textures/Tower/houses.png"),
+		round_unlocked = 1,
 	}
-}
+]
 
-var money: int = 100
+var budget: int = 100
 var health: int = 10
 
-func give_money(n: int) -> void:
-	money += n
-	goldChanged.emit()
+func award_budget(n: int) -> void:
+	budget += n
+	budget_changed.emit()
 
 
 func take_damage() -> void:
---
+++ Scripts/right_menu.gd
@@ -0,0 +1,42 @@
+extends CanvasLayer
+
+
+@onready var budget_label: Label = $side_panel/MarginContainer/Control/budget_label
+@onready var grid_container: GridContainer = $side_panel/MarginContainer/Control/shop_panel/ScrollContainer/GridContainer
+
+var shop_button_scene = load("res://UI/shop_button.tscn")
+
+func _on_budget_changed():
+	budget_label.text = "Tax Dollars: " + str(Globals.budget)
+
+
+func _on_shop_button_pressed(tower_id: String):
+	print(tower_id)
+	TowerHandler.create_tower(tower_id, Vector2(400, 400))
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Globals.budget_changed.connect(_on_budget_changed)
+	
+	for tower_information in Globals.tower_information:
+		var this = shop_button_scene.instantiate()
+		
+		var name_label: Label = this.get_node("Button").get_node("Background").get_node("Name")
+		var price_label: Label = this.get_node("Button").get_node("Background").get_node("Price")
+		var preview_image: TextureRect = this.get_node("Button").get_node("Background").get_node("Preview")
+		
+		name_label.text = str(tower_information.name)
+		price_label.text = str(tower_information.price)
+		preview_image.texture = tower_information.texture
+		
+		#this.get_node("Button").connect("pressed", "self", "_on_shop_button_pressed", [tower_information.id])
+		this.get_node("Button").connect("pressed", _on_shop_button_pressed.bind(tower_information.id))
+		
+		grid_container.add_child(this)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
