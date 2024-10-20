1fdcdef0062029afce451c873ea6c7224b9f7760
Jack8701 <jcarluccio2006@gmail.com>
2024-10-19 23:45:28 -0400

    slight tower remake

--- Scripts/tower.gd
+++ Scripts/tower.gd
@@ -10,25 +10,28 @@ extends Node2D
 
 var last_fire_time: float = 0
 
+# Finds the first enemy that is the furthest down the track, still in the tower range
+func get_first_valid_enemy() -> Node:
+	var last_enemy_index: int = -1
+	while true:
+		var enemy_index: int = EnemyHandler.get_furthest_enemy_index(last_enemy_index + 1)
+		var enemy: Node = EnemyHandler.get_enemy_from_index(enemy_index)
+		if enemy == null:
+			return null
+		
+		last_enemy_index = enemy_index
+		if (enemy.position - position).length() < (damage_range * 144):
+			return enemy
+	
+	return null
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Time.get_ticks_msec() / 1000 - last_fire_time < damage_cooldown:
 		return
 	
-	var fired: bool = false
-	var last_enemy_index: int = -1
-	while true:
-		var enemy_index: int = EnemyHandler.get_furthest_enemy_index(last_enemy_index + 1)
-		var enemy: Node2D = EnemyHandler.get_enemy_from_index(enemy_index)
-		if enemy == null:
-			break
-		
-		last_enemy_index = enemy_index
-		if (enemy.position - position).length() < (damage_range * 144):
-			fired = true
-			enemy.take_damage(damage_rate)
-			break
-	
-	if fired:
+	var enemy: Node = get_first_valid_enemy()
+	if enemy:
+		enemy.take_damage(damage_rate)
 		last_fire_time = Time.get_ticks_msec() / 1000
